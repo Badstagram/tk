05cf2a3687fe097387060b27fda6561bae1a5792
Real4jw <46249942+Real4jw@users.noreply.github.com>
2019-04-18 01:41:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Ticket HT Server`)
+        .addField(`Hey ${message.author.username}!`, `Ticket KB Server`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
